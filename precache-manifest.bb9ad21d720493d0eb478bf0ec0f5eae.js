d0961e919f2f37f257b26ecff6e5455e2b68a23e
malay10 <malaybhakta10@gmail.com>
2020-06-18 10:07:55 -0500

    Updates

---
+++ precache-manifest.bb9ad21d720493d0eb478bf0ec0f5eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcf29136bfe67e3ff6f9b81efbc2cbd",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "009902783be1005dc73e",
+    "url": "/Todo/static/css/main.32570bfc.chunk.css"
+  },
+  {
+    "revision": "02dce05fb7e6954ef2b6",
+    "url": "/Todo/static/js/2.1356fb46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo/static/js/2.1356fb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009902783be1005dc73e",
+    "url": "/Todo/static/js/main.1e134d7e.chunk.js"
+  },
+  {
+    "revision": "162a65c0456d03b31aa2",
+    "url": "/Todo/static/js/runtime-main.e3579041.js"
+  }
+]);
